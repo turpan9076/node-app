e35cd3ad978333b86d7b202c58eb78fcd38d601b
turpan9076 <10x@mbr.nifty.com>
2025-04-21 14:09:17 +0900

    express-hello

---
+++ ex-gen-app/routes/hello.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res, next) => {
+    var data = {
+        title: 'Hello',
+        content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+    };
+    res.render('hello', data);
+});
+module.exports = router;
