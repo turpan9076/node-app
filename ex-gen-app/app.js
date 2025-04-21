840d82ab049175ec863f3a20b6e5dca87a6033af
turpan9076 <10x@mbr.nifty.com>
2025-04-21 17:19:19 +0900

    news

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var newsRouter = require('./routes/news');
 
 const session = require('express-session');
 
@@ -33,6 +34,7 @@ app.use(session(session_opt));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
