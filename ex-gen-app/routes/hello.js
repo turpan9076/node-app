cafad888909604003b664241d10acdbcf52311ab
turpan9076 <10x@mbr.nifty.com>
2025-04-21 14:21:53 +0900

    hello-form

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -4,10 +4,17 @@ router.get('/', (req, res, next) => {
     var name = req.query.name;
     var mail = req.query.mail;
     var data = {
-        title: 'Hello',
-        content: 'あなたの名前は、' + name + '。<br>' +
-            'メールアドレスは、' + mail + 'です。'
+        title: 'Hello!',
+        content: '※何か書いて送信してください。'
     };
     res.render('hello', data);
 });
+router.post('/post', (req, res, next) => {
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + '」と送信しました。'
+    };
+    res.render('hello', data);
+})
 module.exports = router;
