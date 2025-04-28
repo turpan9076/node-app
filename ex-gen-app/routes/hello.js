cc7da06da3b8ba7ae47d587dbe884aa00ed5e46c
turpan9076 <10x@mbr.nifty.com>
2025-04-28 14:16:20 +0900

    db-add

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('mydb.db');
+
 router.get('/', (req, res, next) => {
-    var msg = '※何か書いて送信してください。';
-    if(req.session.message != undefined){
-        msg = "Last Message: " + req.session.message;
-    }
-    var data = {
-        title: 'Hello!',
-        content: msg
-    };
-    res.render('hello', data);
+    db.serialize(() => {
+        var rows = "";
+        db.each("select * from mydata", (err, row) => {
+            if(!err){
+                rows += "<tr><th>" + row.id + "</th><td>" + row.name + "</td></tr>";
+            }
+        }, (err, count) => {
+            if(!err){
+                var data = {
+                    title: "Hello!",
+                    content: rows
+                };
+                res.render('sql', data)
+            }
+        });
+    });
 });
-router.post('/post', (req, res, next) => {
-    var msg = req.body['message'];
-    req.session.message = msg;
+
+router.get('/add', (req, res, next) => {
     var data = {
-        title: 'Hello!',
-        content: "Last Message: " + req.session.message
-    };
-    res.render('hello', data);
-})
+        title: 'Hello/Add',
+        content: '新しいレコードを入力：'
+    }
+    res.render('hello/add',data);
+});
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    db.serialize(() => {
+        db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    });
+    res.redirect('/hello');
+});
+
 module.exports = router;
