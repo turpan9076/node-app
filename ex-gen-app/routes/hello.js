b24ec2c5c02da11101c570e6578188235e42425e
turpan9076 <10x@mbr.nifty.com>
2025-04-28 14:24:39 +0900

    db-show

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -41,4 +41,21 @@ router.post('/add', (req, res, next) => {
     res.redirect('/hello');
 });
 
+router.get('/show', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) =>{
+            if(!err){
+                var data = {
+                    title: 'Hello/show',
+                    content: 'id = ' + id + 'のレコード：',
+                    mydata: row
+                }
+                res.render('hello/show', data);
+            }
+        });
+    });
+});
+
 module.exports = router;
